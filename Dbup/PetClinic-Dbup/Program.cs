440037d54c16bd216cbbaefd9cd09ddaeee963bb
twerthi <twerthi@hotmail.com>
2020-03-20 16:37:02 -0700

    Fixing mysql implementation

--- Dbup/PetClinic-Dbup/Program.cs
+++ Dbup/PetClinic-Dbup/Program.cs
@@ -2,6 +2,10 @@ using System;
 using DbUp;
 using System.Linq;
 using System.Reflection;
+using DbUp.Engine;
+using DbUp.Helpers;
+using DbUp.Support;
+using System.IO;
 
 namespace PetClinic_Dbup
 {
@@ -9,30 +13,52 @@ namespace PetClinic_Dbup
     {
         static int Main(string[] args)
         {
-            var connectionString =
-                args.FirstOrDefault()
-                ?? "Server=(local)\\SqlExpress; Database=MyApp; Trusted_connection=true";
+            var connectionString = args.FirstOrDefault(x => x.StartsWith("--ConnectionString", StringComparison.OrdinalIgnoreCase));
 
-            var upgrader =
+            connectionString = connectionString.Substring(connectionString.IndexOf("=") + 1).Replace(@"""", string.Empty);
+
+            var upgradeEngineBuilder =
                 DeployChanges.To
                     .MySqlDatabase(connectionString)
                     .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                    .LogToConsole()
-                    .Build();
+                    .LogToConsole();
 
-            var result = upgrader.PerformUpgrade();
 
-            if (!result.Successful)
+
+            var upgrader = upgradeEngineBuilder.Build();
+
+            Console.WriteLine("Is upgrade required: " + upgrader.IsUpgradeRequired());
+
+
+            if (args.Any(a => a.StartsWith("--PreviewReportPath", StringComparison.InvariantCultureIgnoreCase)))
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(result.Error);
-                Console.ResetColor();
-                return -1;
-            }
+                // Generate a preview file so Octopus Deploy can generate an artifact for approvals
+                var report = args.FirstOrDefault(x => x.StartsWith("--PreviewReportPath", StringComparison.OrdinalIgnoreCase));
+                report = report.Substring(report.IndexOf("=") + 1).Replace(@"""", string.Empty);
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Success!");
-            Console.ResetColor();
+                var fullReportPath = Path.Combine(report, "UpgradeReport.html");
+
+                Console.WriteLine($"Generating the report at {fullReportPath}");
+
+                upgrader.GenerateUpgradeHtmlReport(fullReportPath);
+            }
+            else
+            {
+                var result = upgrader.PerformUpgrade();
+
+                // Display the result
+                if (result.Successful)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Success!");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(result.Error);
+                    Console.WriteLine("Failed!");
+                }
+            }
             return 0;
         }
     }
