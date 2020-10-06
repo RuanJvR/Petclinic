a9b2381d0c8fdbcda56b0703f4068e1af99ba8d9
twerthi <twerthi@hotmail.com>
2020-10-06 14:29:15 -0700

    Dockerizing petclinic

--- flyway/sql/V1__initDB.sql
+++ flyway/sql/V1__initDB.sql
@@ -1,12 +1,3 @@
-CREATE DATABASE IF NOT EXISTS #{Project.Database.Name};
-
-ALTER DATABASE #{Project.Database.Name}
-  DEFAULT CHARACTER SET utf8
-  DEFAULT COLLATE utf8_general_ci;
-
-
-
-USE #{Project.Database.Name};
 
 CREATE TABLE IF NOT EXISTS vets (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
